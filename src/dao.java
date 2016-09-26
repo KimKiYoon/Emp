09a15df50da960a8c1c6d49ac930f6872f29ecc7
Julietoscarhotelnovember <seedhw@gmail.com>
2016-09-26 16:55:28 +0900

    dao doUpdate 함수 만듬

--- src/dao.java
+++ src/dao.java
@@ -1,4 +1,24 @@
+import java.sql.PreparedStatement;
 
 public class dao {
+	
+	//update: update 함수만
+	public int doUpdate(String empno){
+		int resultrow = 0;
+			try{ 
+	        
+	    		PreparedStatement  pstmt = null;
+	            String sql = "update Emptset set ename, job, mgr, hiredate, sal, comm, deptno where id=?"; 
 
+	            PreparedStatement ps = con.prepareStatement(sql); 
+	            ps.setString(1,empno);       
+	             
+	            resultrow =ps.executeUpdate(); 
+	           
+	            if(ps!=null) ps.close(); 
+	        } catch(Exception e){ 
+	           System.out.println(e.getMessage());
+	        } 
+		return resultrow;
+	}
 }
