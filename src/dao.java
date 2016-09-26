f9dde97105a189bd2543b3c1b01c0c4a146cbd0c
길한종 <seedhw@gmail.com>
2016-09-26 17:07:00 +0900

    dao - doUpdate 수정

--- src/dao.java
+++ src/dao.java
@@ -3,15 +3,22 @@ import java.sql.PreparedStatement;
 public class dao {
 	
 	//update: update 함수만
-	public int doUpdate(String empno){
+	public int doUpdate(int empno, String ename, String job, int mgr, String hiredate, int sal, int comm, int deptno){
 		int resultrow = 0;
 			try{ 
 	        
 	    		PreparedStatement  pstmt = null;
-	            String sql = "update Emptset set ename, job, mgr, hiredate, sal, comm, deptno where id=?"; 
+	            String sql = "update Emptset set ename=?, job=?, mgr=?, hiredate=?, sal=?, comm=?, deptno=? where id=?"; 
 
 	            PreparedStatement ps = con.prepareStatement(sql); 
-	            ps.setString(1,empno);       
+	            ps.setString(1,ename);
+	            ps.setString(2, job);
+	            ps.setInt(3, mgr);
+	            ps.setString(4, hiredate);
+	            ps.setInt(5, sal);
+	            ps.setInt(6, comm);
+	            ps.setInt(7, deptno);
+	            ps.setInt(8, empno);
 	             
 	            resultrow =ps.executeUpdate(); 
 	           
